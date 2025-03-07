f2a2d4933762203f6a158a77ce78e908e70763a8
vilaj46 <vilaj46@gmail.com>
2025-03-07 12:21:57 -0500

    task - db setup
    Setup database following the README

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,12 @@
-import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import db from '../../../db'
+import { advocates } from '../../../db/schema'
+import { advocateData } from '../../../db/seed/advocates'
 
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+	// Uncomment this line to use a database
+	const data = await db.select().from(advocates)
 
-  const data = advocateData;
+	// const data = advocateData;
 
-  return Response.json({ data });
+	return Response.json({ data })
 }
